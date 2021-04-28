b2c7777fd7ad9562f969c8f41300109ae32e11b2
phiathao <phiat11@gmail.com>
2021-04-28 15:00:38 -0500

    cpp guessing game

---
+++ guess-exercise/guessNumber.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main ()
+{
+	bool gameplay =	false;
+	std::string reply = "";
+	std::cout << "Would you like to play a game?" << std::endl << "y/n" << std::endl;
+	if ( reply == "y")
+	{
+
+	}
+		else if (reply =="n")
+	{
+
+	}
+		else
+	{
+		
+	}
+	return 0;
+}
